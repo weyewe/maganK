133b99f96ab4fd15dcfd9fe706e637fce202ff54
weyewe <w.yunnal@gmail.com>
2011-06-22 09:42:14 +0700

    The magank starter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magank::Application.config.secret_token = '7ba93c74ac26613f9486c8c3cb58bf9febc293de1fc4014b7145450c161cfb46ea3c93a765dc3b4ad180803c55ece5acf4f0387d43e04bba3c18a17af3002e5d'
